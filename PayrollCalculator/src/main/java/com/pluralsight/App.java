219f27e030a636dde6dad4e21046aa9c6d2bac1c
DoulaD <Douladoumbia@gmail.com>
2025-04-24 11:03:32 -0400

    Completed Payrolll

--- PayrollCalculator/src/main/java/com/pluralsight/App.java
+++ PayrollCalculator/src/main/java/com/pluralsight/App.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -11,13 +12,23 @@ public class App {
         try {
             FileReader fileReader = new FileReader("src/data/employees.csv");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+            bufferedReader.readLine();
 
             String input;
             while ((input = bufferedReader.readLine()) != null) {
-                System.out.println(input);
-
+                String[] tokens = input.trim().split("\\|");
+                int id = Integer.parseInt(tokens[0]);
+                     String name = tokens[1];
+                double hoursWorked = Double.parseDouble(tokens[2]);
+                double payRate = Double.parseDouble(tokens[3]);
+                Employee employee = new Employee(id,name,hoursWorked,payRate);
+                System.out.printf("ID: %d | Name: %-15s | Gross pay $%.2f%n",
+                        employee.getEmployeeID(),
+                        employee.getName(),
+                        employee.getGrossPay());
             }
         } catch (IOException e) {
+            System.out.println("Error read in file");
             throw new RuntimeException(e);
         }
 
